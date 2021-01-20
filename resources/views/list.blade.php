f97f72eabab623bd5d005ae55f41940701281662
gluck59 <support@motokofr.com>
2021-01-20 15:58:11 +0500

    init

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -11,6 +11,9 @@
     {{-- Font awsome --}}
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
+<style>
+    .ourItem { cursor: pointer; }
+</style>
 <body>
     <br>
     <div class="container">
@@ -18,7 +21,7 @@
             <div class="col-lg-offset-3 col-lg-6">
 				<div class="panel panel-default">
 				  <div class="panel-heading">
-				    <h3 class="panel-title">Ajax ToDo List <a href="#" class="pull-right" data-toggle="modal" data-target="#myModal" id="addNew"><i class="fa fa-plus" aria-hidden="true"></i></a></h3>
+				    <h3 class="panel-title">Purchase List <a href="#" class="pull-right" data-toggle="modal" data-target="#myModal" id="addNew"><i class="fa fa-plus" aria-hidden="true"></i></a></h3>
 				  </div>
 				  <div class="panel-body" id="items">
 				    @if(count($items)>0)
@@ -36,26 +39,22 @@
 				</div>
 			</div>
 
-            <div class="col-lg-2">
-                <input type="text" name="searchItem" id="searchItem" class="form-control" placeholder="Search">
-            </div>
-
             <!-- modal -->
             <div class="modal fade" tabindex="-1" role="dialog" id="myModal">
                 <div class="modal-dialog" role="document"><!-- modal-dialog -->
                     <div class="modal-content"><!-- modal-content -->
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 class="modal-title" id="title">Add new item</h4>
+                            <h4 class="modal-title" id="title">Add new purchase</h4>
                         </div>
                         <div class="modal-body">
                             <input type="hidden" id="id">
                             <p><input type="text" id="addItem" placeholder="write item here ..." class="form-control"></p>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-warning" data-dismiss="modal" style="display:none;" id="delete">Delete</button>
+                            <button type="button" class="btn btn-danger" data-dismiss="modal" style="display:none;" id="delete">Delete</button>
                             <button type="button" class="btn btn-primary" style="display:none;" data-dismiss="modal" id="update">Update</button>
-                            <button type="button" class="btn btn-primary" data-dismiss="modal" id="add">Add item</button>
+                            <button type="button" class="btn btn-success" data-dismiss="modal" id="add">Add item</button>
                         </div>
                     </div><!-- /.modal-content -->
                 </div><!-- /.modal-dialog -->
